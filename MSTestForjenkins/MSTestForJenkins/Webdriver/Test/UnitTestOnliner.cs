02d854fea79cbb3365a196744ca8952e791e78c5
Siarhei <weidarr@gmail.com>
2016-10-31 12:22:21 +0300

    add webdriver test

---
+++ MSTestForjenkins/MSTestForJenkins/Webdriver/Test/UnitTestOnliner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System.Threading;
+
+namespace MSTestForJenkins.Webdriver.Test
+{
+
+    [TestClass]
+    public class UnitTestOnliner
+    {
+        private IWebDriver driver;
+        private const string baseUrl = "https://www.onliner.by/";
+
+        [TestInitialize]
+        public void Init()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(baseUrl);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Thread.Sleep(3000);
+            driver.Quit();
+
+        }
+
+        [TestMethod]
+        public void TestLogin()
+        {
+
+        }
+    }
+}
