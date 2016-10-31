e1ae5bc0d410cd8441f4898d31514994fe8e6e54
Siarhei <weidarr@gmail.com>
2016-10-31 10:53:38 +0300

    add vs project

---
+++ MSTestForJenkins/MSTestForJenkins/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTestForJenkins
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(5, 5);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(2, 5);
+        }
+    }
+}
